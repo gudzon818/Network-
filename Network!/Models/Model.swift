a44426406ae5600c464cf853fd852554b588801c
Kirill <akulenko818@gmail.com>
2023-03-22 09:03:02 +0300

    Пока приходит ошибка так как в JSON словарь

--- Network!/Models/Model.swift
+++ Network!/Models/Model.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct Beer: Codable {
+    let id: Int
+    let name: String
+    let tagline: String
+    let first_brewed: String
+    let description: String
+    let image_url: String
+    let abv: Int
+}
+
+struct BeerData: Codable {
+    let beers: [Beer]
+}
+
+struct Response: Codable {
+    let data: BeerData
+}
--- Network!/ViewControllers/MainViewController.swift
+++ Network!/ViewControllers/MainViewController.swift
@@ -8,14 +8,44 @@
 import UIKit
 
 final class MainViewController: UIViewController {
+    
+    private let url = "https://api.punkapi.com/v2/beers"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBAction func request() {
+       fetchbeers()
     }
     
+}
+
+// MARK: - Networking
+extension MainViewController {
+    func fetchbeers() {
+        guard let url = URL(string: url) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data else {
+                print(error?.localizedDescription ?? "No error description")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            
+            do {
+                let response = try decoder.decode(Response.self, from: data)
+                let beers = response.data.beers
+                print("Number of memes: \(beers.count)")
+                guard let beer = beers.randomElement() else { return }
+                print("Random beer: \(beer)")
+            } catch let error {
+              //  print(error.localizedDescription)
+                print(String(describing: error))
+            }
+        }.resume()
+        
+    }
 }
 
